d119c2efd3af8e2bf91a3ed8771b5891f1150754
superkim0610 <81168407+superkim0610@users.noreply.github.com>
2023-09-19 01:03:24 +0900

    test.py delete

--- test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-a = [0 for _ in range(28)]
-l = [1, 1, 1] + [0 for _ in range(25)]
-r = [0 for _ in range(25)] + [1, 1, 1]
-
-x = np.array([l, l, l] + [a for _ in range(22)] + [r, r, r])
-print(x.shape)
