ec022b2c7e936d3563e450e6022555cca548d27c
superkim0610 <superkim0610@gmail.com>
2023-09-18 22:32:56 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+import struct
+import numpy as np
+
+def load_mnist(path, kind='train'):
+    labels_path = os.path.join(path, '%s-labels-idx1-ubyte' % kind)
+    images_path = os.path.join(path, '%s-images-idx3-ubyte' % kind)
+
+    with open(labels_path, 'rb') as lbpath:
+        magic, n = struct.unpack('>II', lbpath.read(8))
+        labels = np.fromfile(lbpath, dtype=np.uint8)
+
+    with open(images_path, 'rb') as imgpath:
+        magic, num, rows, cols = struct.unpack('>IIII', imgpath.read(16))
+        images = np.fromfile(imgpath, dtype=np.uint8).reshape(len(labels), 784)
+        images = ((images / 255.) - .5) * 2
+
+    return images, labels
+
+X_train, y_train = load_mnist('data/', kind='train')
+print('행 %d, 열: %d' % (X_train.shape[0], X_train.shape[1]))
