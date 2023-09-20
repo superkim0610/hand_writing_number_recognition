dc3c6e6510a472d7a735640eb5fea568b4c7c744
superkim0610 <81168407+superkim0610@users.noreply.github.com>
2023-09-21 02:24:12 +0900

    bug fixed (websocket emitted with private room)

--- _app.py
+++ _app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 import numpy as np
 from learning import nn
 # from visuallization import show_user_img
 
-import random
+active_users = {}
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -13,8 +13,19 @@ socketio = SocketIO(app)
 def index():
     return render_template('index.html')
 
+@socketio.on('connect')
+def handle_connect():
+    user_id = request.sid  # 클라이언트의 세션 ID를 가져옵니다.
+    active_users[user_id] = True
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    user_id = request.sid
+    active_users.pop(user_id, None)
+
 @socketio.on('canvas_data')
 def handle_canvas_data(canvas_data):
+    recipient_id = canvas_data['recipient_id']
     # print('Received canvas data:', canvas_data)  # 캔버스 데이터 출력
     canvas_data = np.array(canvas_data)
     canvas_data = ((canvas_data / 255.) - .5) * 2
@@ -23,7 +34,7 @@ def handle_canvas_data(canvas_data):
     print(result)
     # print(canvas_data)
     
-    socketio.emit('number', {'number': result})
+    socketio.emit('number', {'number': result}, room=recipient_id)
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, host='0.0.0.0', port=80)
